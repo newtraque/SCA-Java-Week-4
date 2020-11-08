3c33d440bd94f10d0500cba5b4d7d2005dc2e0d3
Lateefat Salami <61967044+newtraque@users.noreply.github.com>
2020-11-08 10:45:29 +0100

    Modified methods

--- src/BillingInterface.java
+++ src/BillingInterface.java
@@ -1,37 +1,17 @@
-import java.util.*;
-
 public interface BillingInterface {
-    public void payWithCash();
-    public void payWithCard();
-    public void verifyPayment();
-    public void receipt();
-    public void discount();
+    void verifyPayment();
+    void discount();
 
 }
 
 class Billing implements BillingInterface {
-
-    @Override
-    public void payWithCash() {
-
-    }
-
-    @Override
-    public void payWithCard() {
-
-    }
 
     public void verifyPayment() {
         System.out.println("You have successfully paid! ");
 
     }
 
-    @Override
-    public void receipt() {
 
-    }
-
-    @Override
     public void discount() {
         System.out.println("You have 30% off your payment!");
 
